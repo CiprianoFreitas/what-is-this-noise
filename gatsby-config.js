2bc52f54b811c6d533106b29f371ff71c43f117a
Cipriano Freitas <casf1992@gmail.com>
2018-08-21 19:53:21 +0200

    :lipstick: add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
         background_color: '#FEFF5F',
         theme_color: '#FEFF5F',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-offline',
